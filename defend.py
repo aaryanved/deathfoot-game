4273c8f6d2df955dcd092eeb85a2f11f37e8d802
Aaryan Ved Bhalla <aaryan14.bhalla@gmail.com>
2025-05-21 14:41:53 +0530

    Defence Sequence

---
+++ defend.py
@@ -0,0 +1,15 @@
+import random
+bp = 0
+n = int(input("Enter a number: "))
+if n < 4:
+    m = random.randint(1, 3)
+    if n == m:
+        print("You Lost!")
+        print("")
+        bp += 1
+    else:
+        print("Bot didn't score.")
+        print("")
+else:
+    print("Idiot boy. Game crash because of your foolishness.")
+    exit(0)
