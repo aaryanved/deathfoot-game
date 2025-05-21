d74139a1554efd89c2dab8fde946b563b9247427
Aaryan Ved Bhalla <aaryan14.bhalla@gmail.com>
2025-05-21 15:22:07 +0530

    wrap in fxn

--- Sequences/attack.py
+++ Sequences/attack.py
@@ -1,15 +1,17 @@
 import random
-pp = 0
-n = int(input("Enter a number: "))
-if n < 4:
-    m = random.randint(1, 3)
-    if n == m:
-        print("You Scored!")
-        print("")
-        pp += 1
+
+def attack(pp):
+    n = int(input("Enter a number: "))
+    if n < 4:
+        m = random.randint(1, 3)
+        if n == m:
+            print("You Scored!")
+            print("")
+            pp += 1
+        else:
+            print("You didn't score.")
+            print("")
     else:
-        print("You didn't score.")
-        print("")
-else:
-    print("Idiot boy. Game crash because of your foolishness.")
-    exit(0)
+        print("Idiot boy. Game crash because of your foolishness.")
+        exit(0)
+    return pp
--- Sequences/defend.py
+++ Sequences/defend.py
@@ -1,15 +1,17 @@
 import random
-bp = 0
-n = int(input("Enter a number: "))
-if n < 4:
-    m = random.randint(1, 3)
-    if n == m:
-        print("You Lost!")
-        print("")
-        bp += 1
+
+def defend(bp):
+    n = int(input("Enter a number: "))
+    if n < 4:
+        m = random.randint(1, 3)
+        if n == m:
+            print("You Lost!")
+            print("")
+            bp += 1
+        else:
+            print("Bot didn't score.")
+            print("")
     else:
-        print("Bot didn't score.")
-        print("")
-else:
-    print("Idiot boy. Game crash because of your foolishness.")
-    exit(0)
+        print("Idiot boy. Game crash because of your foolishness.")
+        exit(0)
+    return bp
